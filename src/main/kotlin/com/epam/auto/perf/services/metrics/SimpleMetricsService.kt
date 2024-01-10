dc0c079c648c21ffc034f93204c60a6b647b4abc
bodyangug <aseevbogdan5@gmail.com>
2024-01-10 16:47:28 +0200

    fix: resolve issue with cpu usage on Windows

--- src/main/kotlin/com/epam/auto/perf/services/metrics/SimpleMetricsService.kt
+++ src/main/kotlin/com/epam/auto/perf/services/metrics/SimpleMetricsService.kt
@@ -1,9 +1,9 @@
 package com.epam.auto.perf.services.metrics
 
+import com.sun.management.OperatingSystemMXBean
 import mu.KotlinLogging
 import java.lang.management.ManagementFactory
 import java.lang.management.MemoryMXBean
-import java.lang.management.OperatingSystemMXBean
 import java.math.BigDecimal
 import java.math.RoundingMode
 
@@ -27,7 +27,7 @@ class SimpleMetricsService(urlConnection: String) {
     fun getCPUUsage(): Double {
         logger.info { "Retrieve cpu usage." }
         return metricsRetriever.retrieveJMXMetric<OperatingSystemMXBean>(ManagementFactory.OPERATING_SYSTEM_MXBEAN_NAME)
-            .systemLoadAverage
+            .processCpuLoad
     }
 
 }
